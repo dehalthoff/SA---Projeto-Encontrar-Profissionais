fd3c140bef6bd529998eabeeb6b1bbb8984835bf
André Althoff <dehalthoff@hotmail.com>
2021-04-01 14:43:26 -0300

    Tela Pesquisa dos profissionais

--- SA - Projeto Encontrar Profissionais/TelaPesquisa.Designer.cs
+++ SA - Projeto Encontrar Profissionais/TelaPesquisa.Designer.cs
@@ -29,20 +29,265 @@ namespace SA___Projeto_Encontrar_Profissionais
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Profissional = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Profissao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Formacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Preco = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbProcurarPeloBairro = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbLongitude = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbLatitude = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLoadIntoMap = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Profissional,
+            this.Profissao,
+            this.Estado,
+            this.Cidade,
+            this.Formacao,
+            this.Preco,
+            this.Descricao});
+            this.dataGridView1.Location = new System.Drawing.Point(-7, 244);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(777, 276);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Text = "dataGridView1";
+            // 
+            // Profissional
+            // 
+            this.Profissional.HeaderText = "Profissional";
+            this.Profissional.Name = "Profissional";
+            this.Profissional.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Profissional.Width = 102;
+            // 
+            // Profissao
+            // 
+            this.Profissao.HeaderText = "Profissão";
+            this.Profissao.Name = "Profissao";
+            this.Profissao.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Profissao.Width = 102;
+            // 
+            // Estado
+            // 
+            this.Estado.HeaderText = "Estado";
+            this.Estado.Name = "Estado";
+            this.Estado.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Estado.Width = 102;
+            // 
+            // Cidade
+            // 
+            this.Cidade.HeaderText = "Cidade";
+            this.Cidade.Name = "Cidade";
+            this.Cidade.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Cidade.Width = 102;
+            // 
+            // Formacao
+            // 
+            this.Formacao.HeaderText = "Formação";
+            this.Formacao.Name = "Formacao";
+            this.Formacao.Width = 102;
+            // 
+            // Preco
+            // 
+            this.Preco.HeaderText = "Valor Hora / Preço";
+            this.Preco.Name = "Preco";
+            this.Preco.Width = 102;
+            // 
+            // Descricao
+            // 
+            this.Descricao.HeaderText = "Descrição";
+            this.Descricao.Name = "Descricao";
+            this.Descricao.Width = 102;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(403, 128);
+            this.maskedTextBox1.Mask = "$000\\.00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(324, 23);
+            this.maskedTextBox1.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(405, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 19);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Procurar por Valor";
+            // 
+            // cbProcurarPeloBairro
+            // 
+            this.cbProcurarPeloBairro.AutoSize = true;
+            this.cbProcurarPeloBairro.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cbProcurarPeloBairro.ForeColor = System.Drawing.Color.White;
+            this.cbProcurarPeloBairro.Location = new System.Drawing.Point(30, 176);
+            this.cbProcurarPeloBairro.Name = "cbProcurarPeloBairro";
+            this.cbProcurarPeloBairro.Size = new System.Drawing.Size(358, 25);
+            this.cbProcurarPeloBairro.TabIndex = 16;
+            this.cbProcurarPeloBairro.Text = "Apenas procurar pelo mesmo bairro que eu";
+            this.cbProcurarPeloBairro.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(51, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 31);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Voltar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // cmbLongitude
+            // 
+            this.cmbLongitude.FormattingEnabled = true;
+            this.cmbLongitude.Items.AddRange(new object[] {
+            "50"});
+            this.cmbLongitude.Location = new System.Drawing.Point(30, 128);
+            this.cmbLongitude.Name = "cmbLongitude";
+            this.cmbLongitude.Size = new System.Drawing.Size(314, 23);
+            this.cmbLongitude.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(30, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 19);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Cidade";
+            // 
+            // cmbLatitude
+            // 
+            this.cmbLatitude.FormattingEnabled = true;
+            this.cmbLatitude.Items.AddRange(new object[] {
+            "100"});
+            this.cmbLatitude.Location = new System.Drawing.Point(403, 76);
+            this.cmbLatitude.Name = "cmbLatitude";
+            this.cmbLatitude.Size = new System.Drawing.Size(324, 23);
+            this.cmbLatitude.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(405, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 19);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Estado";
+            // 
+            // btnLoadIntoMap
+            // 
+            this.btnLoadIntoMap.BackColor = System.Drawing.Color.Yellow;
+            this.btnLoadIntoMap.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLoadIntoMap.Location = new System.Drawing.Point(159, 207);
+            this.btnLoadIntoMap.Name = "btnLoadIntoMap";
+            this.btnLoadIntoMap.Size = new System.Drawing.Size(103, 31);
+            this.btnLoadIntoMap.TabIndex = 11;
+            this.btnLoadIntoMap.Text = "Pesquisar";
+            this.btnLoadIntoMap.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 23);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(30, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 19);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Profissão";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Yellow;
+            this.label5.Location = new System.Drawing.Point(215, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(323, 31);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Pesquisa dos Profissionais";
             // 
             // TelaPesquisa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(654, 523);
+            this.ClientSize = new System.Drawing.Size(753, 523);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbProcurarPeloBairro);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbLongitude);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbLatitude);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLoadIntoMap);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "TelaPesquisa";
             this.Text = "TelaPesquisa";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Profissional;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Profissao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Estado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Formacao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Preco;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descricao;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox cbProcurarPeloBairro;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbLongitude;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbLatitude;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLoadIntoMap;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
     }
 }
